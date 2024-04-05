29127663f5e827f8c267f048e8595c7f14d115b8
VladLen43 <136459377+VladLen43@users.noreply.github.com>
2024-04-05 15:49:25 +0300

    ssss

--- controllers/TodoController.js
+++ controllers/TodoController.js
@@ -51,25 +51,56 @@ export const create = async (req, res) => {
      }
 }
 
-export const update = async (req, res) => {
-    const todoId = req.params.id;
+// export const update = async (req, res) => {
+    
+//     const todoId = req.params.id;
+  
+//       await TodoModel.findOneAndUpdate(
+//         {
+//           _id: todoId,
+//         },
+//         {
+//             title: req.body.title,
+//             imageUrl: req.body.imageUrl
 
-    await TodoModel.updateOne({
-        _id: todoId,
-    },{
-        title: req.body.title,
-        imageUrl: req.body.imageUrl,
-        user: req.userId,
-    },)
-    res.json({
-        success: true,
-    })
-}
+//         }).then((doc, err) => {
+//             if (err) {
+//                 console.log(err);
+//                 return res.status(500).json({
+//                   message: 'Не удалось удалить todo',
+//                 });
+//               }
+      
+//               if (!doc) {
+//                 return res.status(404).json({
+//                   message: 'Todo не найдена',
+//                 });
+//               }
+      
+//               res.json({
+//                 success: true,
+//               });
+//         }).catch((reject) => {
+//             console.log(reject);
+//             res.status(500).json({
+//               message: 'Не удалось получить todo',
+//             });
+//         })     
+// }
+
+// 
+export const update = async (req, res) => {
+    const postId = req.params.id;
+    TodoModel.findOneAndUpdate(
+        { _id: postId } ,{ title: 't'},{ returnDocument: "After" } )
+        .then(doc => res.json(doc))
+        .catch(err => res.status(500).json({ message: "Статья не найдена" }))    
+  };
 
 export const remove = async (req, res) => {
       const todoId = req.params.id;
   
-      TodoModel.findOneAndDelete(
+     TodoModel.findOneAndDelete(
         {
           _id: todoId,
         }).then((doc, err) => {
